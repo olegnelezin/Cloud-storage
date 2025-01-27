e1729bda2f860c897842d20d9de7c2989334841a
Oleg Nelezin <astravsu@gmail.com>
2025-01-27 12:35:04 +0300

    feat: add user entity

---
+++ src/main/java/ru/nelezin/storage/entity/User.java
@@ -0,0 +1,23 @@
+package ru.nelezin.storage.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "user")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
