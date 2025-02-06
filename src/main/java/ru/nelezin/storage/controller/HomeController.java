7a2b40f1e4e746fdfbb5151716b30aca02b76b0c
Oleg Nelezin <astravsu@gmail.com>
2025-02-06 23:50:27 +0300

    feat: adding test variant of home page

--- src/main/java/ru/nelezin/storage/controller/HomeController.java
+++ src/main/java/ru/nelezin/storage/controller/HomeController.java
@@ -1,23 +1,26 @@
 package ru.nelezin.storage.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.nelezin.storage.entity.User;
+import ru.nelezin.storage.dto.FileDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RequestMapping("/")
 @Controller
 public class HomeController {
 
-
     @GetMapping
-    public String homePage(@AuthenticationPrincipal User user, Model model) {
-        if (user != null) {
-
-        }
+    public String homePage(Model model) {
+        List<FileDto> userFiles = new ArrayList<>();
+        userFiles.add(new FileDto("1", "11", "111", "1111"));
+        userFiles.add(new FileDto("2", "22", "222", "2222"));
+        userFiles.add(new FileDto("3", "33", "333", "3333"));
+        model.addAttribute("files", userFiles);
         return "home";
     }
 }
--- src/main/java/ru/nelezin/storage/dto/FileDto.java
+++ src/main/java/ru/nelezin/storage/dto/FileDto.java
@@ -1,5 +1,14 @@
 package ru.nelezin.storage.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FileDto {
 
     private String filename;
